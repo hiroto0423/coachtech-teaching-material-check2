5a28c1ad1a06200000c85919fefac1d01a9dbded
hiroto0423 <rohito.0423-tkh@outlook.jp>
2023-01-31 13:44:22 +0900

    データを作成しよう：シーディング編-CRUD処理を学ぼう：データの追加

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Author;
+
+class AuthorController extends Controller
+{
+    public function index()
+    {
+        $authors = Author::all();
+        return view('index', ['authors' => $authors]);
+    }
+    public function add(){
+        return view('add');
+    }
+
+    public function create(Request $request){
+        $form = $request->all();
+        Author::create($form);
+        return redirect('/');
+    }
+}
