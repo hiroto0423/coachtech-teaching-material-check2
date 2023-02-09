e0a6d6016e204de77aabfec3bd144e0db8e65060
“Hayato <hayabusa06mochi@gmail.com>
2023-02-09 23:19:13 +0900

    （現教材）ファクトリ

--- database/seeders/AuthorsTableSeeder.php
+++ database/seeders/AuthorsTableSeeder.php
@@ -3,46 +3,18 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use App\Models\Author;
 
 class AuthorsTableSeeder extends Seeder
 {
-  /**
-  * Run the database seeds.
-  *
-  * @return void
-  */
+/**
+* Run the database seeds.
+*
+* @return void
+*/
   public function run()
   {
-    $param = [
-      'name' => 'tony',
-      'age' => 35,
-      'nationality' => 'American'
-    ];
-    Author::create($param);
-    $param = [
-      'name' => 'jack',
-      'age' => 20,
-      'nationality' => 'British'
-    ];
-    Author::create($param);
-    $param = [
-      'name' => 'sara',
-      'age' => 45,
-      'nationality' => 'Egyptian'
-    ];
-    Author::create($param);
-    $param = [
-      'name' => 'saly',
-      'age' => 31,
-      'nationality' => 'Chinese'
-    ];
-    Author::create($param);
-    $param = [
-      'name' => 'taro',
-      'age' => 55,
-      'nationality' => 'Japanese'
-    ];
-    Author::create($param);
+    Author::factory()->count(3)->create();
   }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,13 +6,13 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        $this->call(AuthorsTableSeeder::class);
-    }
+/**
+* Seed the application's database.
+*
+* @return void
+*/
+  public function run()
+  {
+    \App\Models\Author::factory(10)->create();
+  }
 }
